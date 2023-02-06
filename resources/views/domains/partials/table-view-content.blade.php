ee8a46c6063224d08105e5883bdd535c37443df8
Milen Karaganski <milen@blacktiehost.com>
2023-02-06 14:15:40 +0200

    minor

--- resources/views/domains/partials/table-view-content.blade.php
+++ resources/views/domains/partials/table-view-content.blade.php
@@ -20,24 +20,24 @@
         <td class="text-sm text-gray-900 font-light px-2 py-4 whitespace-nowrap">{{ $domain->website }}</td>
         <td class="text-sm text-gray-900 font-light px-2 py-4 whitespace-nowrap">
             <button type="submit"
-                    class="inline-flex items-center px-2 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest
+                    class="inline-flex items-center px-2 py-1 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest
                     hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
                 <a href="{{ route('records') }}?domain_id={{ $domain->id }}" class="bg-blue-500 p-2 text-white hover:shadow-lg text-xs font-thin">{{ __('View Records') }}</a>
             </button>
             <button type="submit"
-                    class="inline-flex items-center px-2 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest
+                    class="inline-flex items-center px-2 py-1 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest
                     hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
                 <a href="{{ $domain->id }}/edit" class="bg-blue-500 p-2 text-white hover:shadow-lg text-xs font-thin">{{ __('Edit') }}</a>
             </button>
-            <div class="inline-flex items-center px-2 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest
-            hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+            <div class="inline-flex items-center px-2 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest
+                        hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">
                 <form action="{{route('domains.destroy',[$domain->id])}}" method="post">
                     @csrf
                     {{method_field('delete')}}
                     <button type="submit"
-                            class="inline-flex items-center px-2 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest
-                            hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
-                    {{ __('Delete') }}
+                            class="inline-flex items-center px-2 py-1 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest
+                        hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                        {{ __('Delete') }}
                     </button>
                 </form>
             </div>
--- resources/views/records/partials/table-view-content.blade.php
+++ resources/views/records/partials/table-view-content.blade.php
@@ -25,11 +25,13 @@
             <td class="text-sm text-gray-900 font-light px-6 py-4">{{ substr($record->password, 0, 10) }}</td>
             <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                 <button type="submit"
-                        class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                        class="inline-flex items-center px-2 py-1 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest
+                        hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
                     <a href="#" class="bg-blue-500 p-2 text-white hover:shadow-lg text-xs font-thin">{{ __('Edit') }}</a>
                 </button>
                 <button type="submit"
-                        class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                        class="inline-flex items-center px-2 py-1 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest
+                        hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">
                     <a href="#" class="bg-red-500 p-2 text-white hover:shadow-lg text-xs font-thin">{{ __('Delete') }}</a>
                 </button>
             </td>
